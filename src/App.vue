38d1f2981d8e4e9eb3fb2586337c32aeced4a670
seunghwan94 <seunghwan94@gmail.com>
2024-05-27 10:37:05 +0000

    first

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="background-select-color">
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
 
 <style>
+body{
+  margin: 0;
+}
+
+div{
+  box-sizing: border-box;
+}
+
+.background-select-color{
+  position: fixed;
+  padding: 20px;
+  width: 100%;
+  height: 100%;
+  background: #a7c4e0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
